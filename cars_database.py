d030ecc31c23800b9dadef30ea4487bdaaa42645
Dian VK <diankinanev@abv.bg>
2022-12-13 18:27:19 +0200

    Creating the App GUI with KivyMD

---
+++ cars_database.py
@@ -0,0 +1,49 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivymd.uix.screen import Screen
+Window.size = (300,500)
+
+screen_helper = """
+Screen:
+    BoxLayout:
+        orientation : 'vertical'
+        MDTopAppBar :
+            title: "Автокомплекс 'ВЕРОС'"   
+            font_size: '20sp'
+            left_action_items: [["menu",lambda x:app.navigation_draw()]]
+            
+        MDLabel:
+            text: 'Here will be the input and storing data'
+            halign: 'center'
+        
+        MDBottomAppBar:
+
+            MDTopAppBar:
+                title: "Брой коли: {cars_len}"
+                font_size: '1sp'
+                icon: "tools"
+                type: "bottom"
+                left_action_items: [["toolbox", lambda x: x]]
+                mode: "end"
+
+    
+"""
+
+
+class cars_database(MDApp):
+
+    def build(self):
+        self.theme_cls.primary_palette = 'Red'
+        screen = Builder.load_string(screen_helper)
+        return screen
+
+
+    def navigation_draw(self):
+        print("Navigation")
+
+
+
+
+
+cars_database().run()

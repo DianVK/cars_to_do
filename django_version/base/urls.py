336a133ebf47caeaf2a17bd764280626c9979561
DianVK <diankinanev@abv.bg>
2023-01-22 22:44:35 +0200

    create login form

--- django_version/base/urls.py
+++ django_version/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from base.views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from base.views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- django_version/base/views.py
+++ django_version/base/views.py
@@ -5,7 +5,18 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+
 from base.models import Task
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 
 class TaskList(ListView):

74eb9a98a54a15867074fca9b734d4d546c9c715
DianVK <diankinanev@abv.bg>
2023-01-22 21:31:35 +0200

    button for adding cars

--- django_version/base/urls.py
+++ django_version/base/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from base.views import TaskList, TaskDetail
+from base.views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
--- django_version/base/views.py
+++ django_version/base/views.py
@@ -1,6 +1,10 @@
+from audioop import reverse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from base.models import Task
 
 
@@ -13,3 +17,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')

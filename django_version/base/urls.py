35f8b0c9f7034c4516d7fa2885eebf6ad3636093
DianVK <diankinanev@abv.bg>
2023-01-22 20:43:32 +0200

    add detailview

--- django_version/base/urls.py
+++ django_version/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from base.views import TaskList
+from base.views import TaskList, TaskDetail
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 
 ]

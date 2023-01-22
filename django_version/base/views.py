f35837f7980850cc2d8ea185a506c7adcd2178a0
DianVK <diankinanev@abv.bg>
2023-01-22 22:09:32 +0200

    delete button

--- django_version/base/views.py
+++ django_version/base/views.py
@@ -2,7 +2,7 @@ from audioop import reverse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from base.models import Task
@@ -26,4 +26,9 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')

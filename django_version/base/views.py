dc0719d2557229476bc88ff5ac270aaa1683fdab
DianVK <diankinanev@abv.bg>
2023-01-23 10:56:14 +0200

    count cars to do

--- django_version/base/views.py
+++ django_version/base/views.py
@@ -45,7 +45,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False)
+        context['count'] = context['tasks'].filter(complete=False).count()
         
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
